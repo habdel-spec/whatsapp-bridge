38ffa6c9c12d1deb427024dfe5e8a8a48c402d05
habdel-spec <habdel@noon.com>
2026-09-15 23:35:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ let isReady = false;
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { args: ['--no-sandbox', '--disable-setuid-sandbox'] }
+    puppeteer: {
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
+    }
 });
 
 client.on('qr', (qr) => {
